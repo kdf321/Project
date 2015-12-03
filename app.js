ece94b0606caf0a4baf58c630de7c764f65a60c4
kdf321 <nayatrei01@naver.com>
2015-12-03 20:13:13 +0900

    second commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (app.get('env') === 'development') {
 app.locals.moment = require('moment');
 
 // mongodb connect
-mongoose.connect('mongodb://user:asdasd@ds041394.mongolab.com:41394/nodewp');
+mongoose.connect('mongodb://flzmfltm:dkfcksghlrhks12@ds049864.mongolab.com:49864/flzmfltm');
 mongoose.connection.on('error', console.log);
 
 // uncomment after placing your favicon in /public
